fcd48003b14c671df4ead11ff7b1d312f495c21a
JoseJavierM <90474992+JoseJavierM@users.noreply.github.com>
2022-11-22 00:02:24 +0100

    uPDATED

--- topologyP4.py
+++ topologyP4.py
@@ -5,33 +5,23 @@ from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 
-def topologyP4():
-    "Create custom topo."
-    # Initialize topology
-    net = Mininet(topo=None, build=False, link=TCLink, controller=None)
-    # Add hosts and switches
-    Host1 = net.addHost( 'h1' , ip='10.0.1.100/24',mac='00:00:00:00:00:01',defaultRoute="via 10.0.1.1")
-    Host2 = net.addHost('h2', ip='10.0.2.100/24',mac='00:00:00:00:00:02',defaultRoute="via 10.0.2.1")
-    Host3 = net.addHost('h3', ip='10.0.3.100/24',mac='00:00:00:00:00:03',defaultRoute="via 10.0.3.1")
+class LeafSpine( Topo ):
+	def __init__( self ):
 
-    centralSwitch = net.addSwitch('s1')
-    # Add links
+		# Initialize topology
+		Topo.__init__( self )
+        "Create custom topo."
+        # Add hosts and switches
+        Host1 = net.addHost( 'h1' , ip='10.0.1.100/24',mac='00:00:00:00:00:01',defaultRoute="via 10.0.1.1")
+        Host2 = net.addHost('h2', ip='10.0.2.100/24',mac='00:00:00:00:00:02',defaultRoute="via 10.0.2.1")
+        Host3 = net.addHost('h3', ip='10.0.3.100/24',mac='00:00:00:00:00:03',defaultRoute="via 10.0.3.1")
+
+        centralSwitch = net.addSwitch('s1')
+        # Add links
     
-    net.addLink( Host1, centralSwitch )
-    net.addLink( Host2, centralSwitch )
-    net.addLink( Host3, centralSwitch )
+        net.addLink( Host1, centralSwitch )
+        net.addLink( Host2, centralSwitch )
+        net.addLink( Host3, centralSwitch )
 
-    net.addLink(aloneHost7, centralSwitch)
-
-    #Ejecución de la red
-    net.start()
-
-    CLI(net)
-
-    net.stop()
-
-
-if __name__ == "__main__":
-    setLogLevel('info')
-    topologyP4()
+topos = { 'mytopo': ( lambda: LeafSpine() ) }
 
